42e0320aa274f49865eea2be53e307a953035603
KAS1DET <134573399+KasidetPha@users.noreply.github.com>
2025-04-30 11:54:25 +0700

    update

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import '../css/style.css'
 
 const Pagination = ({ currentPage, totalPages, onPageChange}) => {
     const pages = useMemo(() => {
@@ -10,25 +11,40 @@ const Pagination = ({ currentPage, totalPages, onPageChange}) => {
     }, [totalPages])
 
     return (
-        <div className="flex justify-center items-center gap-4 mt-4">
-            <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1} className="bg-blue-500 text-white px-4 py-2 rounded disabled:bg-gray-400 disabled:cursor-not-allowed transition-all">
-                Prev
-            </button>
+        <div className=" flex justify-center items-center">
+            <div className='pagination mt-10'>
 
-            <div className='flex space-x-1'>
-                {pages.map((page) => (
-                    <button key={page} 
-                    onClick={() => onPageChange(page)}
-                    className={`px-3 py-1 rounded-md transition-all w-10 text-center ${
-                        page === currentPage ? 'bg-blue-700 text-white' : 'bg-gray-200 hover:bg-gray-300'}`}>
-                        {page}
-                    </button>
-                ))}
+                <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1} className="bg-blue-500 text-white px-4 py-2 rounded disabled:bg-gray-400 disabled:cursor-not-allowed transition-all">
+                    &lt;
+                </button>
+
+                {/* <div className='pagination flex space-x-1'>
+                    {pages.map((page) => (
+                        <button key={page} 
+                        onClick={() => onPageChange(page)}
+                        className={`px-3 py-1 rounded-md transition-all w-10 text-center ${
+                            page === currentPage ? 'bg-blue-700 text-white' : 'bg-gray-200 hover:bg-gray-300'}`}>
+                            {page}
+                        </button>
+                    ))}
+                </div> */}
+
+
+                <div className=' flex space-x-1'>
+                    {pages.map((page) => (
+                        <button key={page} 
+                        onClick={() => onPageChange(page)}
+                        className={`px-3 py-1 rounded-md transition-all w-10 text-center ${
+                            page === currentPage ? 'active' : ''}`}>
+                            {page}
+                        </button>
+                    ))}
+                </div>
+
+                <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages} className="bg-blue-500 text-white px-4 py-2 rounded disabled:bg-gray-400 disabled:cursor-not-allowed transition-all">
+                    &gt;
+                </button>
             </div>
-
-            <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages} className="bg-blue-500 text-white px-4 py-2 rounded disabled:bg-gray-400 disabled:cursor-not-allowed transition-all">
-                Next
-            </button>
         </div>
     )
 }
